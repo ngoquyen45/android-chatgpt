8d0373d26294aab44d9760974b6c71027beb425f
Quyền Ngô <ngoquyen45@gmail.com>
2023-02-25 08:36:44 +0700

    add back events

--- app/src/main/java/com/infisdev/chatgpt/MainActivity.kt
+++ app/src/main/java/com/infisdev/chatgpt/MainActivity.kt
@@ -3,8 +3,14 @@ package com.infisdev.chatgpt
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.webkit.*
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.google.android.material.progressindicator.LinearProgressIndicator
@@ -13,6 +19,15 @@ import com.google.android.material.progressindicator.LinearProgressIndicator
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var linearProgressIndicator: LinearProgressIndicator
+    private val handler by lazy { Handler(Looper.getMainLooper()) }
+    private var backPressedOnce = false
+    private val exitToast by lazy {
+        Toast.makeText(
+            applicationContext,
+            "Press back again to exit",
+            Toast.LENGTH_SHORT
+        )
+    }
 
     @SuppressLint("SetJavaScriptEnabled", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +42,7 @@ class MainActivity : AppCompatActivity() {
                     linearProgressIndicator.isVisible = true
                     super.onPageStarted(view, url, favicon)
                 }
+
                 override fun onPageFinished(view: WebView, url: String) {
                     linearProgressIndicator.isVisible = false
                     super.onPageFinished(view, url)
@@ -39,20 +55,50 @@ class MainActivity : AppCompatActivity() {
             }
             loadUrl("https://chat.openai.com")
         }
+
+        setupOnBack()
+    }
+
+    private fun setupOnBack() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.url?.contains(CHAT) == true) {
+                    handleExit()
+                    return
+                }
+                if (webView.canGoBack()) webView.goBack()
+                else {
+                    handleExit()
+                }
+            }
+        })
+    }
+
+    private fun handleExit() {
+        if (backPressedOnce) finish()
+        backPressedOnce = true
+        exitToast.show()
+        handler.postDelayed({
+            backPressedOnce = false
+            exitToast.cancel()
+        }, 2000)
     }
 
     private fun customWeb(url: String) {
         if (!url.contains(CHAT)) return
 
         // Loại bỏ phần thừa của web
-        webView.evaluateJavascript("""setInterval(function() {
+        webView.evaluateJavascript(
+            """setInterval(function() {
             var element_hide = document.querySelector('#__next > div.overflow-hidden.w-full.h-full.relative > div > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient > div');
             if (element_hide) { element_hide.style.display = 'none'; }
             var element_padding = document.querySelector('#__next > div.overflow-hidden.w-full.h-full.relative > div > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient > form');
             if (element_padding) { element_padding.style.paddingBottom = '0.5rem'; }
-        }, 1000);""".trimIndent()) { value -> Log.d("Evaluate Javascript", value) }
+        }, 1000);""".trimIndent()
+        ) { value -> Log.d("Evaluate Javascript", value) }
 
-        webView.evaluateJavascript("""setInterval(function() {
+        webView.evaluateJavascript(
+            """setInterval(function() {
             var selector = document.querySelector("#__next > div.overflow-hidden.w-full.h-full.relative > div > div");
             if (!selector) return;
             if (selector.querySelector("#my-button")) return;
@@ -88,7 +134,8 @@ class MainActivity : AppCompatActivity() {
               location.reload();
             });
             selector.appendChild(button);
-        }, 1000);""".trimIndent()) { value -> Log.d("Evaluate Javascript", value) }
+        }, 1000);""".trimIndent()
+        ) { value -> Log.d("Evaluate Javascript", value) }
     }
 
     companion object {
